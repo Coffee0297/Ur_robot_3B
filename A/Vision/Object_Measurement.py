a6b618c5207154ddd03cc5f02f0890a17028c6f5
Dakkefar <76039537+Dakkefar@users.noreply.github.com>
2020-12-17 21:19:18 +0100

    Test med masks og edge detection ny

---
+++ A/Vision/Object_Measurement.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+import
+
+webcam = False
+path = 'c:\\Users\\Pc\\Desktop\\3. Semester\\Vision\\Ressourcer\\Billeder\\a4.jpg'
+cap = cv.VideoCapture(0)
+cap.set(10,160)
+cap.set(3,480)
+cap.set(4,640)
+
+while True:
+    if webcam:success,img = cap.read()
+    else: img = cv.imread(path)
+
+
+
+    img = cv.resize(img,(0,0),None, 0.5, 0.5)
+
+    cv.imshow("Original", img)
+    if cv.waitKey(0) == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
