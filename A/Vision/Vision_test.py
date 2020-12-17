cc12cace53360acc61d4584bae1cf6225f1301f6
Dakkefar <76039537+Dakkefar@users.noreply.github.com>
2020-12-17 10:43:49 +0100

    Test med masks og edge detection ny

--- A/Vision/Vision_test.py
+++ A/Vision/Vision_test.py
@@ -2,23 +2,29 @@ import cv2 as cv
 import numpy as np
 
 cap = cv.VideoCapture(1)
-
+# Video size = (480, 640, 3)
 while True:
     ret, img = cap.read()
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     blank = np.zeros(img.shape, dtype='uint8')
+
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    height, width, channels = img.shape
+
     # cv.imshow("Blank", blank)
 
-    mask = cv.circle(blank, (img.shape[1]//2), img.shape[0]//2), 100, 255, -1)
+    mask = cv.rectangle(blank, (width//2, height//2),(width//2 - 100, height//2 - 100), (255, 255, 255), -1)
     cv.imshow("Mask", mask)
 
-    masked = cv.bitwise_and(img, img, mask= mask)
+    masked = cv.bitwise_and(gray, gray, mask=mask)
     cv.imshow("Masked image", masked)
 
     # cv.imshow("Grå Video", gray)
 
     gaussianblur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
     # cv.imshow('gblur', gaussianblur)
+
+    print("Video size: ", img.shape)
 
 
 
