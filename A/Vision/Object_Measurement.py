60c1982e799c2c531c498eb8f1211eab0b92d8f9
Dakkefar <76039537+Dakkefar@users.noreply.github.com>
2020-12-17 21:40:14 +0100

    Øvelse fra youtube video 1

--- A/Vision/Object_Measurement.py
+++ A/Vision/Object_Measurement.py
@@ -1,7 +1,6 @@
 import cv2 as cv
 import numpy as np
-import utlis
-
+import
 webcam = False
 path = 'c:\\Users\\Pc\\Desktop\\3. Semester\\Vision\\Ressourcer\\Billeder\\a4.jpg'
 cap = cv.VideoCapture(0)
--- A/Vision/utlis.py
+++
@@ -1,8 +0,0 @@
-import cv2 as cv
-import numpy as np
-
-def getContours(img,cThr=[100,100],showCanny=False):
-    imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    imgBlur = cv.GaussianBlur(imgGray,(5,5),1)
-    imgCanny = cv.Canny(imgBlur,cThr[0],cThr[1])
-    if showCanny: cv.imshow('Canny',imgCanny)
