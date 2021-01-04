109fc0a5999785ad5e24dfb196c3b36ed3d6a215
Dakkefar <76039537+Dakkefar@users.noreply.github.com>
2021-01-04 09:26:35 +0100

    Sidste version inden min PC gik kold. 04-01-2021 kl. 09:26

--- A/Vision/Object_Measurement.py
+++ A/Vision/Object_Measurement.py
@@ -3,18 +3,18 @@
 
 import cv2 as cv
 import numpy as np
-
+import custommodule as cm
 
 import os
 import sys
 
 
 file_dir = os.path.dirname('customModule')
-sys.path.append('c:\\Users\\Pc\\PycharmProjects\\Ur_robot_3B\\A\\Vision\\customModule.py')
+sys.path.append('/A/Vision/custommodule.py')
 
 
 
-webcam = False
+webcam = True
 path = 'c:\\Users\\Pc\\Desktop\\3. Semester\\Vision\\Ressourcer\\Billeder\\a4_klods_1.jpg'
 cap = cv.VideoCapture(1)
 cap.set(10,160)
@@ -22,69 +22,9 @@ cap.set(3,1920)
 cap.set(4,1080)
 scale = 3
 wP = 210 *scale
-hP = 279 *scale
-
-def getContours(img, cThr=[100, 100], showCanny=False, cannyResize=False, minArea = 500, filter=0, draw = False): # minArea er standard = 100
-    imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    imgBlur = cv.GaussianBlur(imgGray, (5, 5), 1)
-    imgCanny_Original = cv.Canny(imgBlur, cThr[0], cThr[1])
-    # imgCanny_Resized = cv.resize(imgCanny_Original.copy(),(0, 0), None, 0.4, 0.4)
-    kernel = np.ones((5,5))
-    imgDial = cv.dilate(imgCanny_Original, kernel, iterations=3)
-    imgThres = cv.erode(imgDial, kernel, iterations=2)
+hP = 148.5 *scale
 
 
-
-    if showCanny: cv.imshow('Canny', imgCanny_Original)
-    if cannyResize: cv.imshow('Resized Canny', imgThres)
-
-    contours, hierarchy = cv.findContours(imgThres,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
-    finalContours = []
-    for i in contours:
-        area = cv.contourArea(i)
-        if area > minArea:
-            peri = cv.arcLength(i,True)
-            approx = cv.approxPolyDP(i,0.02*peri,True)
-            bbox = cv.boundingRect(approx)
-            if filter > 0:
-                if len(approx) == filter:
-                    finalContours.append([len(approx),area,approx,bbox,i])
-            else:
-                finalContours.append([len(approx),area,approx,bbox,i])
-    finalContours = sorted(finalContours,key= lambda x:x[1], reverse=True)
-
-    if draw:
-        for con in finalContours:
-            cv.drawContours(img,con[4],-1,(0,0,255),3)
-
-    return img, finalContours
-
-def reorder(myPoints):
-    print(myPoints.shape)
-    myPointsNew = np.zeros_like(myPoints)
-    myPoints = myPoints.reshape((4,2))
-    add = myPoints.sum(1)
-    myPointsNew[0] = myPoints[np.argmin(add)]
-    myPointsNew[3] = myPoints[np.argmax(add)]
-    diff = np.diff(myPoints, axis=1)
-    myPointsNew[1] = myPoints[np.argmin(diff)]
-    myPointsNew[2] = myPoints[np.argmax(diff)]
-
-    return myPointsNew
-
-def warpImg(img, points, w, h, pad = 20):
-    # print("Points: ", points)
-    points = reorder(points)
-    pts1 = np.float32(points)
-    pts2 = np.float32([[0,0],[w,0],[0,h],[w,h]])
-    matrix = cv.getPerspectiveTransform(pts1,pts2)
-    imgWarp = cv.warpPerspective(img, matrix, (w,h))
-    imgWarp = imgWarp[pad:imgWarp.shape[0]-pad, pad:imgWarp.shape[1]-pad]
-
-    return imgWarp
-
-def findDis(pts1, pts2):
-    return ((pts2[0]-pts1[0])**2 + (pts2[1]-pts1[1])**2)**0.5
 
 
 while True:
@@ -92,19 +32,19 @@ while True:
     if webcam: success,img = cap.read()
     else: img = cv.imread(path)
 
-    img, conts = getContours(img, minArea=50000, showCanny= True, filter=4)
+    img, conts = cm.getContours(img, minArea=5000, showCanny= True, filter=4)
 
     if len(conts) != 0:
         biggest = conts[0][2]
         print("Biggest: ", biggest)
-        imgWarp = warpImg(img, biggest, wP, hP)
-        img2, conts2 = getContours(imgWarp, minArea=2000, filter=4, cThr=[50,50], draw=False)
+        imgWarp = cm.warpImg(img, biggest, wP, hP)
+        img2, conts2 = cm.getContours(imgWarp, minArea=200, filter=4, cThr=[50,50], draw=False) # minArea er standard 2000
         if len(conts2) != 0:
             for obj in conts2:
                 cv.polylines(img2, [obj[2]], True,(0,255,0), 2)
-                nPoints = reorder(obj[2])
-                nW = round((findDis(nPoints[0][0]//scale,nPoints[1][0]//scale)/10),1)
-                nH = round((findDis(nPoints[0][0]//scale,nPoints[2][0]//scale)/10),1)
+                nPoints = cm.reorder(obj[2])
+                nW = round((cm.findDis(nPoints[0][0]//scale,nPoints[1][0]//scale)/10),1)
+                nH = round((cm.findDis(nPoints[0][0]//scale,nPoints[2][0]//scale)/10),1)
                 cv.arrowedLine(img2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[1][0][0], nPoints[1][0][1]),
                                (255, 0, 255), 3, 8, 0, 0.05)
                 cv.arrowedLine(img2, (nPoints[0][0][0], nPoints[0][0][1]), (nPoints[2][0][0], nPoints[2][0][1]),
