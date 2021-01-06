a9a4d99165806fd514d3c2efab00385fe343b79f
cbl73 <71754999+cbl73@users.noreply.github.com>
2021-01-06 21:50:13 +0100

    updated

---
+++ C/Vision/urlib.py
@@ -0,0 +1,10 @@
+
+
+class Position:
+    def __init__(self, x, y, z, rx, ry, rz):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.rx = rx
+        self.ry = ry
+        self.rz = rz
