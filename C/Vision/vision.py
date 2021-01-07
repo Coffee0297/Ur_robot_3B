4f64596d8bf4c7f43bf05bf72c072143d8dc77fc
cbl73 <71754999+cbl73@users.noreply.github.com>
2021-01-07 15:22:26 +0100

    updated

--- C/Vision/vision.py
+++ C/Vision/vision.py
@@ -8,7 +8,7 @@ import cv2 as cv
 # Vinduesnavn
 cv.namedWindow('Tracking', cv.WINDOW_NORMAL)
 
-class Color:
+class Trackbar:
     def __init__(self, lh, ls, lv, hh, hs, hv):     # HSV værdier - low/high
         self.lh = lh     # creates atribute called hl and takes in parameter hl
         self.ls = ls
@@ -16,7 +16,6 @@ class Color:
         self.hh = hh
         self.hs = hs
         self.hv = hv
-
 
     def nothing(self):  # definere nothing for at den kan blive ignoreret i cv.createTrackbar
         lh = cv.getTrackbarPos("Low_H", "Tracking")
@@ -27,6 +26,15 @@ class Color:
         vh = cv.getTrackbarPos("High_V", "Tracking")
         return lh, ls, lv, hh, hs, hv
 
+class Color(Trackbar):
+    def create(self):
+        cv.createTrackbar("Low_H", "Tracking", self.lh, 180, Color.nothing)
+        cv.createTrackbar("Low_S", "Tracking", self.ls, 255, Color.nothing)
+        cv.createTrackbar("Low_V", "Tracking", self.lv, 255, Color.nothing)
+        cv.createTrackbar("High_H", "Tracking", self.hh, 180, Color.nothing)
+        cv.createTrackbar("High_S", "Tracking", self.hs, 255, Color.nothing)
+        cv.createTrackbar("High_V", "Tracking", self.hv, 255, Color.nothing)
+
     def printValue(self):    # til debugging
         print('HSV values')
         print(self.lh)
@@ -36,25 +44,33 @@ class Color:
         print(self.hs)
         print(self.hv)
 
-default = Color(0,0,0,0,0,0)
-print(default.printValue())         # debugging
 
-greenDefault = Color(53,74,66,96,225,185)
-print(greenDefault.printValue())    # debugging
+default = Trackbar(0,0,0,0,0,0)
+redDefault = Trackbar(0,0,0,24,255,88)
+greenDefault = Trackbar(53,74,66,96,225,185)
+blueDefault = Trackbar(0,0,65,171,234,110)
 
-cv.createTrackbar("Low_H", "Tracking", default.lh, 180, Color.nothing)
-cv.createTrackbar("Low_S", "Tracking", default.ls, 255, Color.nothing)
-cv.createTrackbar("Low_V", "Tracking", default.lv, 255, Color.nothing)
-cv.createTrackbar("High_H", "Tracking", default.hh, 180, Color.nothing)
-cv.createTrackbar("High_S", "Tracking", default.hs, 255, Color.nothing)
-cv.createTrackbar("High_V", "Tracking", default.hv, 255, Color.nothing)
+red1 = Color.create(redDefault)
+#green1 = Color.create(greenDefault)
+
+print(Color.printValue(redDefault))           # debugging
+
+# greenDefault = Trackbar(53,74,66,96,225,185)
+# print(greenDefault.printValue())    # debugging
+
+# cv.createTrackbar("Low_H", "Tracking", greenDefault.lh, 180, Color.nothing)
+# cv.createTrackbar("Low_S", "Tracking", default.ls, 255, Color.nothing)
+# cv.createTrackbar("Low_V", "Tracking", default.lv, 255, Color.nothing)
+# cv.createTrackbar("High_H", "Tracking", default.hh, 180, Color.nothing)
+# cv.createTrackbar("High_S", "Tracking", default.hs, 255, Color.nothing)
+# cv.createTrackbar("High_V", "Tracking", default.hv, 255, Color.nothing)
 
 
-# blueDefault = Color(0,0,65,171,234,110)
+
 # blueDefault.printValue()          # debugging
 
-# redDefault = Color(0,0,0,24,255,88)
-# redDefault.printValue()           # debugging
-
-
+while True:
+    key = cv.waitKey(30)
+    if key == ord('q') or key == 27:
+        break
 
