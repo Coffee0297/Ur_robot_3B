e4cf2099f6e5b38475ab93ec65da241a0659548a
cbl73 <71754999+cbl73@users.noreply.github.com>
2021-01-07 09:45:05 +0100

    updated

---
+++ C/Vision/vision.py
@@ -0,0 +1,18 @@
+# tester klasser og objekter
+#-------------------------------------------------------
+class Position:
+    def __init__(self, hl, hh, sl, sh, vl, vh):
+        self.hl = hl     # creates atribute called x and takes in parameter x
+        self.y = y
+        self.z = z
+        self.rx = rx
+        self.ry = ry
+        self.rz = rz
+
+    def printPosition(self):    # til debugging
+        print(self.x)
+        print(self.y)
+        print(self.z)
+        print(self.rx)
+        print(self.ry)
+        print(self.rz)
