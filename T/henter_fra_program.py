76f64270897cfd989a4e4a49a3d1e2b2b677f7e2
Tonny Møller <43273303+Coffee0297@users.noreply.github.com>
2021-01-10 22:54:26 +0100

    update

---
+++ T/henter_fra_program.py
@@ -0,0 +1,23 @@
+#robotprogram
+import Program
+
+x = []
+y = []
+rad = []
+
+rad, x, y = Program.main()
+
+print('Radianer: ',rad)
+print('X Coordinat i centerpunkt: ',x)
+print('Y coordinat i centerpunkt: ',y)
+
+
+
+
+
+
+    # her bevæger robotten sig......
+
+
+
+
